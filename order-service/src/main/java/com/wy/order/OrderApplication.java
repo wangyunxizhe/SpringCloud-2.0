6a5dc4314ed8a7f218d30f58afa7174e0abaf048
王渊 <543456229@qq.com>
2023-01-05 17:18:39 +0800

    Ribbon负载规则以及饥饿加载

--- order-service/src/main/java/com/wy/order/OrderApplication.java
+++ order-service/src/main/java/com/wy/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package com.wy.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,8 +19,16 @@ public class OrderApplication {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    /* 改变负载规则方案一：全局改变。无论是user-service，还是pay-service/xxx-service都会遵循这里的负载规则（方案二在配置文件中） */
+    /*@Bean
+    public IRule myRule() {
+        //Ribbon共有7种负载策略，默认是轮询。
+        //此处将ribbon默认使用的 轮询策略 改为 随机策略
+        return new RandomRule();
+    }*/
 
 }
