3ffef47c49b095ca4d9c49d36f6673bc1d001ca4
王渊 <543456229@qq.com>
2023-01-06 09:32:26 +0800

    使用Nacos替换Eureka

--- order-service/src/main/java/com/wy/order/OrderApplication.java
+++ order-service/src/main/java/com/wy/order/OrderApplication.java
@@ -5,6 +5,7 @@ import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
