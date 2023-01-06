58af6685905835e98859a1c73c25131bb8c3e7fa
王渊 <543456229@qq.com>
2023-01-06 17:11:23 +0800

    基于Feign的微服务调用方式

--- order-service/src/main/java/com/wy/order/OrderApplication.java
+++ order-service/src/main/java/com/wy/order/OrderApplication.java
@@ -1,17 +1,18 @@
 package com.wy.order;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.wy.order.mapper")
 @SpringBootApplication
+@EnableFeignClients
 public class OrderApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/wy/order/service/OrderService.java
+++ order-service/src/main/java/com/wy/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.wy.order.service;
 
+import com.wy.order.feignService.UserClient;
 import com.wy.order.mapper.OrderMapper;
 import com.wy.order.pojo.Order;
 import com.wy.order.pojo.User;
@@ -16,11 +17,20 @@ public class OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserClient userClient;
+
     public Order queryOrderById(Long orderId) {
         // 查询订单
         Order order = orderMapper.findById(orderId);
-        String url="http://user-service/user/"+order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+
+        //不使用Feign，通过restTemplate调用provider
+        /*String url="http://user-service/user/"+order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);*/
+
+        //使用Feign，代替restTemplate调用provider
+        User user = userClient.findById(order.getUserId());
+
         order.setUser(user);
         // 返回
         return order;
